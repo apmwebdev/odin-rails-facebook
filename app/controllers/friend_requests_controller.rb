e821941adc21373ed46fffa02cc2ef6bde1a2be3
Austin Miller <austinmillerwebdev@gmail.com>
2023-01-13 16:15:59 -0500

    Pare down friend request functionality
    Remove JSON responds_to + views. Remove edit + update methods, views, and
    routes

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -15,35 +15,14 @@ class FriendRequestsController < ApplicationController
     @friend_request = FriendRequest.new
   end
 
-  # GET /friend_requests/1/edit
-  def edit
-  end
-
   # POST /friend_requests or /friend_requests.json
   def create
     @friend_request = FriendRequest.new(friend_request_params)
 
-    respond_to do |format|
-      if @friend_request.save
-        format.html { redirect_to friend_request_url(@friend_request), notice: "Friend request was successfully created." }
-        format.json { render :show, status: :created, location: @friend_request }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @friend_request.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /friend_requests/1 or /friend_requests/1.json
-  def update
-    respond_to do |format|
-      if @friend_request.update(friend_request_params)
-        format.html { redirect_to friend_request_url(@friend_request), notice: "Friend request was successfully updated." }
-        format.json { render :show, status: :ok, location: @friend_request }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @friend_request.errors, status: :unprocessable_entity }
-      end
+    if @friend_request.save
+      format.html { redirect_to friend_request_url(@friend_request), notice: "Friend request was successfully created." }
+    else
+      format.html { render :new, status: :unprocessable_entity }
     end
   end
 
@@ -51,20 +30,17 @@ class FriendRequestsController < ApplicationController
   def destroy
     @friend_request.destroy
 
-    respond_to do |format|
-      format.html { redirect_to friend_requests_url, notice: "Friend request was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    format.html { redirect_to friend_requests_url, notice: "Friend request was successfully destroyed." }
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_friend_request
-      @friend_request = FriendRequest.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_friend_request
+    @friend_request = FriendRequest.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def friend_request_params
-      params.require(:friend_request).permit(:sender_id, :recipient_id, :status)
-    end
+  # Only allow a list of trusted parameters through.
+  def friend_request_params
+    params.require(:friend_request).permit(:sender_id, :recipient_id, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "/landing_page", to: "home#landing_page"
-  get "/newsfeed", to: "home#newsfeed"
-  resources :friend_requests
+  resources :friend_requests, except: [:edit, :update]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  authenticated :user do
+    root to: 'home#newsfeed', as: :authenticated_root
+  end
   root "home#landing_page"
 end
