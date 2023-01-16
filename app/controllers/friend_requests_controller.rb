1fc3999da6adbec2326dd2ec56bd14e12ec492e0
Austin Miller <austinmillerwebdev@gmail.com>
2023-01-15 20:58:10 -0500

    Add basic notifications UI

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -13,6 +13,7 @@ class FriendRequestsController < ApplicationController
     @friend_request.status = "pending"
 
     if @friend_request.save
+      create_friend_request_notif(@friend_request)
       redirect_to friend_request_url(@friend_request),
         notice: "Friend request was successfully created."
     else
@@ -31,6 +32,7 @@ class FriendRequestsController < ApplicationController
     user2 = @friend_request.recipient
     friendship = Friendship.new(user1:, user2:)
     if friendship.save
+      create_friendship_notif(friendship)
       @friend_request.destroy!
       redirect_to friend_requests_url, notice: "Accepted friend request!"
     end
@@ -45,5 +47,27 @@ class FriendRequestsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def friend_request_params
     params.require(:friend_request).permit(:sender_id, :recipient_id)
+  end
+
+  def create_friendship_notif(friendship)
+    n = Notification.new
+    recipient_name = helpers.name(friendship.user2)
+    n.title = "#{recipient_name} accepted your friend request!"
+    n.body = "Hooray!"
+    n.status = "unread"
+    n.user_id = friendship.user1_id
+    n.notifiable = friendship
+    n.save
+  end
+
+  def create_friend_request_notif(friend_request)
+    n = Notification.new
+    sender_name = helpers.name(friend_request.sender)
+    n.title = "New friend request from #{sender_name}"
+    n.body = "Would you like to accept this friend request?"
+    n.status = "unread"
+    n.user_id = friend_request.recipient_id
+    n.notifiable = friend_request
+    n.save
   end
 end
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
 
   # GET /notifications or /notifications.json
   def index
-    @notifications = Notification.all
+    @notifications = Notification.where(user_id: current_user.id)
   end
 
   # GET /notifications/1 or /notifications/1.json
