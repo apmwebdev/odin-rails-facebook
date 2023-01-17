b97dd65d571abc9db733417036ae3bd5f3d29028
Austin Miller <austinmillerwebdev@gmail.com>
2023-01-17 18:17:54 -0500

    Add likes for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :commenter, class_name: "User"
   has_many :likes, as: :likeable
+  has_many :likers, through: :likes, source: :user
   has_one :notification, as: :notifiable
 
   attr_accessor :turbo_target
